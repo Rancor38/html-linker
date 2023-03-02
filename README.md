50f300045d15207499e9b5bfc8f252fef46b63e2
Rancor38 <zak.rancor@hotmail.com>
2023-03-02 12:55:22 -0800

    re

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# html-linker
+#html-linker
+
+Fork and clone repo, inside of the module npm i -g
+
+Use: npx html-linker, to fork/clone/generate a new html linker
---
+++ bin/index.js
@@ -0,0 +1,6 @@
+#! /usr/bin/env node
+console.log("Hello, World!");
+const shell = require('shelljs')
+const path = '.'
+shell.cd(path)
+shell.exec('git clone https://github.com/Rancor38/html-linker')
