9b3ea8b2214c56b6c80f5809f913b709f50894e3
Rancor38 <zak.rancor@hotmail.com>
2023-03-03 06:52:05 -0800

    Edit to readme

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 #html-linker
 
-Fork and clone repo, inside of the module npm i -g
-
-Use: npx html-linker, to fork/clone/generate a new html linker
+Use: npx html-linker <project-name>, to fork/clone/generate a new html linker
--- bin/index.js
+++ bin/index.js
@@ -3,5 +3,9 @@ console.log("Hello, World!");
 const shell = require('shelljs')
 const path = '.'
 shell.cd(path)
-shell.exec('git clone https://github.com/Rancor38/html-linker')
-shell.exec('git remote rm origin')
+let args = process.argv.slice(2)
+if (args.length === 0) {
+    args = "new-html-linker"
+}
+console.log(args)
+shell.exec(`git clone https://github.com/Rancor38/html-linker ${args ? args : "new-html-linker"}`)
